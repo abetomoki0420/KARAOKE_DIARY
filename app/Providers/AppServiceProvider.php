53adcf7baac1cea9c74f296137762eb2bbf1aaaf
chachamame <kimi.jack.of.all.trades@gmail.com>
2018-11-23 17:23:01 +0900

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if($this->app->environment() == 'production'){
-        //   URL::forceScheme('https');
-        // }
+        if($this->app->environment() == 'production'){
+          URL::forceScheme('https');
+        }
     }
 
     /**
