6a68e08bf19e99d80ef3e8a8e8d08203ebbccaa6
chachamame <kimi.jack.of.all.trades@gmail.com>
2018-11-23 12:42:54 +0900

    fix

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://karaoke-diary.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
